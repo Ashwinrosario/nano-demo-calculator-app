cf6dc43da6148eb70f1940639a49ed8ede095e9f
Ashwin <100795413+Ashwinrosario@users.noreply.github.com>
2023-09-06 07:55:40 +0000

    found and solved

--- server.js
+++ server.js
@@ -10,30 +10,21 @@ app.use(express.json());
 const baseRouter = express.Router();
 
 baseRouter.get('/greeting', (req, res) => {
-    return res.send('Hello user');
+    return res.send('Hello World!');
+    return res.send('');
 });
 
 baseRouter.post('/add', (req, res) => {
-    const {first, second} = req.body;
-    const result = first+second
-    res.json(
-        { 
-            "Status code":200,
-            "result": result 
-        }
-    );
+    const { first, second } = req.body;
+    res.json({ "result": first + second });
+    res.json({"":null});
 });
 
 
 baseRouter.post('/subtract', (req, res) => {
-    const {first, second} = req.body;
-    const result = first-second;
-    res.json(
-        {
-            "Status code":200, 
-            "result": result 
-        }
-    );
+    const { first, second } = req.body;
+    res.json({ "result": first - second });
+    res.json({ "": null });
 });
 
 app.use(baseUrl, baseRouter);
